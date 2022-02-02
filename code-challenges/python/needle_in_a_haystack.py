8a0f95f6e607944c2b6b735f4a390f6281794b97
craigmcg <craigmcgowan15@gmail.com>
2022-02-01 22:30:16 -0500

    python kata 1

---
+++ code-challenges/python/needle_in_a_haystack.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
+
+def find_needle(haystack):
+    # your code here
+    needle = str(haystack.index("needle"))
+    return "found the needle at position "+ needle
+    
