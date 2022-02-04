768ab2ff9430773a0d7f0cdd4e81f135e3e09628
craigmcg <craigmcgowan15@gmail.com>
2022-02-04 14:30:53 -0500

    area or perimeter complete

---
+++ code-challenges/python/area-or-perimeter.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/5ab6538b379d20ad880000ab/train
+
+def area_or_perimeter(l , w):
+    # return your answer
+    print(l,w)
+    if l == w: return l**2
+    return l*2+w*2
