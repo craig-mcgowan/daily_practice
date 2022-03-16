33fdf2238b51340697cc0a3f3efac2f567592ec4
Craig McGowan <craigmcgowan15@gmail.com>
2022-03-15 23:21:33 -0400

    js practice

--- code-challenges/javascript/opposites.js
+++ code-challenges/javascript/opposites.js
@@ -1,1 +1,6 @@
-https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+//https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+
+function lovefunc(flower1, flower2) {
+  // moment of truth
+  return (flower1 + flower2) % 2 !== 0;
+}
