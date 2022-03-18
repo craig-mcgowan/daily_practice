4efb453842485baf6f58bbf671a775e16bfbce4a
Craig McGowan <craigmcgowan15@gmail.com>
2022-03-18 15:13:53 -0400

    javascript practice 031822

--- code-challenges/javascript/reflection_points.js
+++ code-challenges/javascript/reflection_points.js
@@ -1,1 +1,6 @@
 // https://www.codewars.com/kata/57bfea4cb19505912900012c/train/javascript
+
+function symmetricPoint(p, q) {
+  console.log(p, q);
+  return [q[0] + q[0] - p[0], q[1] + q[1] - p[1]];
+}
