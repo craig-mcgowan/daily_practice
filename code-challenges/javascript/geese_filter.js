5f136a8a628f01bb8f39c9e2e4763399edfd2ed0
Craig McGowan <craigmcgowan15@gmail.com>
2022-03-17 21:43:28 -0400

    completed practice

--- code-challenges/javascript/geese_filter.js
+++ code-challenges/javascript/geese_filter.js
@@ -1,7 +1,6 @@
 function gooseFilter(birds) {
+  //https://www.codewars.com/kata/57ee4a67108d3fd9eb0000e7/train/javascript
   var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
 
-  // return an array containing all of the strings in the input array except those that match strings in geese
-
-  return birds.filter(bird => !geese.includes(bird))
+  return birds.filter((bird) => !geese.includes(bird));
 }
