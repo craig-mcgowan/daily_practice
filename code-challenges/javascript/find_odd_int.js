ae7747c63967a980ca9bfc227a3aa732e8f772a4
Craig McGowan <craigmcgowan15@gmail.com>
2022-03-21 23:02:23 -0400

    js practice

--- code-challenges/javascript/find_odd_int.js
+++ code-challenges/javascript/find_odd_int.js
@@ -1,6 +1,23 @@
 //www.codewars.com/kata/54da5a58ea159efa38000836/train/javascript
 
-https: function findOdd(A) {
-  //happy coding!
-  return 0;
+function findOdd(A) {
+  A.sort((a, b) => a - b);
+  console.log(A);
+  let oddInt = 0;
+  A.forEach((num, i) => {
+    if (num !== A[i - 1]) {
+      console.log("num: ", num);
+      let x = i;
+      let count = 0;
+      while (num === A[x]) {
+        count++;
+        x++;
+      }
+      console.log("count: ", count);
+      if (count % 2 !== 0) {
+        oddInt = num;
+      }
+    }
+  });
+  return oddInt;
 }
