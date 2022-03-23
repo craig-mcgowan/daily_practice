298f0585447246c2a8e63b9cf7fcdb9acf79765e
Craig McGowan <craigmcgowan15@gmail.com>
2022-03-22 22:45:12 -0400

    js practice

--- code-challenges/javascript/first_nonrepeater.js
+++ code-challenges/javascript/first_nonrepeater.js
@@ -2,4 +2,13 @@
 
 function firstNonRepeatingLetter(s) {
   // Add your code here
+  const mapS = new Map()
+
+  for (let i = 0; i < s.length; i++) {
+    mapS.set(s[i].toLowerCase(),i)
+
+  }
+  for (char in mapS) {
+    if (char.length = 1) return char
+  }
 }
