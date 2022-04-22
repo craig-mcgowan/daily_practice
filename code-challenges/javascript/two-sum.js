86c03a5ebabdf2cfb78ee44ee71ae7556bbc2a59
Craig McGowan <craigmcgowan15@gmail.com>
2022-04-22 12:06:38 -0400

    two-sum

--- code-challenges/javascript/two-sum.js
+++ code-challenges/javascript/two-sum.js
@@ -1,19 +1,36 @@
-// https://leetcode.com/problems/two-sum/submissions/
-
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
-    let arr = []
-    nums.forEach((num, index)=> {
-        if (target/2 !== num) { 
-            const ans = target - num
-            console.log(ans)
-            const ansIndex = nums.findIndex(x => x===ans)
-            if (ansIndex !== -1) 
-        }
-    })
-    return arr
+//const arr1 = [2,7,11,15]
+//const target = 9
+const twoSum = function (nums, target) {
+  console.log(nums, target);
+  const comp = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (comp.hasOwn(nums[i])) {
+      return [comp[nums[i]], i];
+    }
+    comp[target - nums[i]] = i;
+  }
 };
+
+/*
+Running through the above...
+loop 1
+    comp = {}
+    i=0
+    nums[1]=2
+    comp does not have a property of 2 so the if statement does not run
+    comp[9-2]=0
+    comp{7:0}
+loop 2
+    comp{7:0}
+    i=1
+    nums[1]=7
+    comp does have a property of 7 !
+    returns [comp[7], 1]
+    returns [0,1]
+*/
