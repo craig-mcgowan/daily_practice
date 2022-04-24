ea06ed287df18b0517bb8440c9450e603658ef67
Craig McGowan <craigmcgowan15@gmail.com>
2022-04-24 12:52:30 -0400

    js practice

--- code-challenges/javascript/palindromeNumber.js
+++ code-challenges/javascript/palindromeNumber.js
@@ -1,9 +1,14 @@
 //https://leetcode.com/problems/palindrome-number/
 
-/**
+
+/*
  * @param {number} x
  * @return {boolean}
  */
+
 var isPalindrome = function(x) {
+    if (x<0) {false}
+    x = x.toString()
+    return x=== x.split("").reverse().join("")
     
 };
