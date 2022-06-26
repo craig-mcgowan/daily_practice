d67e3788383597e5ea3bed2f2c0357bba9631ec4
Craig McGowan <craigmcgowan15@gmail.com>
2022-06-26 16:58:48 -0400

    csharp basics

---
+++ learning/csharp/helloworld.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Craig McGowan");    
+     }
+  }
+}
+ 
+using System;
+
+namespace GettingInput
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("How old are you?");
+      string input = Console.ReadLine();
+      Console.WriteLine($"You are {input} years old!");
+    }
+  }
+}
+
