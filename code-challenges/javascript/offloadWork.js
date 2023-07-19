413d8d162213dc397449c1eb49a4142195abeb92
Craig McGowan <craigmcgowan15@gmail.com>
2023-07-18 23:00:01 -0400

    daily js practice

--- code-challenges/javascript/offloadWork.js
+++ code-challenges/javascript/offloadWork.js
@@ -1,1 +1,9 @@
-function workNeeded(projectMinutes, freelancers) {}
+function workNeeded(projectMinutes, freelancers) {
+  console.log("projectMinutes: " + projectMinutes);
+  console.log("freelancers: " + freelancers);
+  const totalFreelancerTime = freelancers
+    .reduce((acc, freelancer) => acc + freelancer[0] * 60 + freelancer[1],
+    0
+  )
+  console.log(totalFreelancerTime);
+}
